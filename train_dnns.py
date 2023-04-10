55ec105f624002a0bc0b2377204396cee10ac32f
周钰坤 <ykzhou@stu.xidian.edu.cn>
2023-04-10 16:19:23 +0800

    Update train_dnns.py

--- train_dnns.py
+++ train_dnns.py
@@ -9,6 +9,12 @@ import numpy as np
 import argparse
 from torch.nn.utils import clip_grad_norm_
 
+# os.environ["WANDB_MODE"] = "offline"
+# os.environ["WANDB_SILENT"] = "true"
+os.environ['WANDB_API_KEY'] = 'ec5d114180c22f7ec57e35cf5d7370f4c6ffe839'
+import wandb
+wandb.login()
+
 def get_args_parser():
     # get the parameters from the terminal
     parser = argparse.ArgumentParser(description='Generalization in Deep Learning')
@@ -47,11 +53,18 @@ def get_args_parser():
     
     # learning rate schedule parameters
     parser.add_argument('--lr_scheduler', type=str, default='alpha_plan')
+    parser.add_argument("--notes", type=str, default=None)
+    
+    parser.add_argument('--beta', default=1, type=float,
+                        help='hyperparameter beta')
+    parser.add_argument('--cutmix_prob', default=0, type=float,
+                        help='cutmix probability')
     
     args = parser.parse_args()
     return args
 
-def train_one_epoch(net, optimizer, criterion, train_loader, args):
+from torchvision.utils import make_grid
+def train_one_epoch(net, optimizer, criterion, train_loader, args, epoch):
     # metrics
     acc_meter = AverageMeter()
     acc5_meter = AverageMeter()
@@ -59,13 +72,44 @@ def train_one_epoch(net, optimizer, criterion, train_loader, args):
     
     # train
     net.train()
-    for i, (images, labels) in enumerate(train_loader):
-        images = images.to(device)
-        labels = labels.to(device)
+    for i, (images, target) in enumerate(train_loader):
+        input = input.cuda()
+        target = target.cuda()
+
+        r = np.random.rand(1)
+        if args.beta > 0 and r < args.cutmix_prob:
+            # generate mixed sample
+            # pdb.set_trace()
+            lam = np.random.beta(1, 1) / 2 
+            rand_index = torch.randperm(input.size()[0]).cuda()
+            target_a = target
+            target_b = target[rand_index]
+            
+            height = 32
+            width = 32 
+            n_channels = 3
+            
+            rand = torch.rand(height, width)
+            masks = (rand < lam).long().cuda()
+            
+            input[:, :] =  input[:, :] * (1 - masks) # + input[rand_index, :]  * masks 
+            
+            output = net(input)
+            loss = criterion(output, target_a) 
+            
         
-        logits = net(images)
-        loss = criterion(logits, labels)
-        
+        else:
+            # compute output
+            output = net(input)
+            loss = criterion(output, target)
+            
+        input_data = wandb.Table(columns=['Image'])
+        if epoch == 0 and i < 1:
+            for num in range(10):
+                image = wandb.Image(make_grid(input[num]))
+                input_data.add_data(image)
+            wandb.log({'input': input_data})
+
         optimizer.zero_grad()
         loss.backward()
         
@@ -74,7 +118,7 @@ def train_one_epoch(net, optimizer, criterion, train_loader, args):
         
         optimizer.step() 
         
-        acc, acc5 = accuracy(logits, labels, topk=(1,5))
+        acc, acc5 = accuracy(output, target, topk=(1,5))
         acc_meter.update(acc)
         acc5_meter.update(acc5)
         loss_meter.update(loss.item())
@@ -105,23 +149,46 @@ def evaluate(net, criterion, eval_loader, args):
         
     return acc_meter.avg, acc5_meter.avg, loss_meter.avg
 
+import datetime
 def train_net_for_classification(net, optimizer, criterion, train_loader, eval_loader, lr_scheduler, args):
     print("Training network for classification")
     alpha_plan = [0.01] * 60 + [0.001] * 40
+    
+    nowtime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    run = wandb.init(project='sparsemix', name=nowtime, notes=args.notes, save_code=True)
+    wandb.config = {
+        'batch_size': args.batch_size,
+        'arch': args.arch,
+        'load': args.load,
+        'save': args.save,
+        'criterion': args.criterion,
+        'lr_scheduler': args.lr_scheduler,
+        'optimizer': args.optimizer,
+        'learning_rate': args.learning_rate,
+        
+    }
 
     for epoch in range(1, args.epochs):
-        train_acc, train_acc5, train_loss = train_one_epoch(net, optimizer, criterion, train_loader, args)
+        train_acc, train_acc5, train_loss = train_one_epoch(net, optimizer, criterion, train_loader, args, epoch)
         test_acc, test_acc5, test_loss = evaluate(net, criterion, eval_loader, args)
         
         print("""Iteration: [{:03d}/{:03d}]
               train: @acc: {:.3f}, @acc5: {:.3f}, @loss: {:.3f}
               test: @acc: {:.3f}, @acc5: {:.3f}, @loss: {:.3f}""".format(epoch, args.epochs, train_acc, train_acc5, train_loss, test_acc, test_acc5, test_loss))
         
+        wandb.log({'train_loss': train_loss, 'train_acc': train_acc,'top1': test_acc})
+        
         if args.lr_scheduler == 'alpha_plan':
             adjust_learning_rate(optimizer, alpha_plan[epoch])
         else:
             lr_scheduler.step()
         save_model(args.save, net, optimizer, args)
+        
+    code = wandb.Artifact('python', type='code')
+    code.add_file('train_dnns.py')
+    wandb.log_artifact(code)
+    
+    wandb.finish()
         
 def train_dnns(args):
     # initialize 
