04848c762a25f317ff2f82cfc6fa129183eba75a
周钰坤 <ykzhou@stu.xidian.edu.cn>
2023-04-10 16:03:02 +0800

    update

--- main.sh
+++
@@ -1,6 +0,0 @@
-python model_evaluate.py --n_workers 4 
-python model_evaluate.py --n_workers 4 --noise gaussian --erasing_ratio 0.02 
-python model_evaluate.py --n_workers 4 --noise gaussian --erasing_ratio 0.05
-python model_evaluate.py --n_workers 4 --noise gaussian --erasing_ratio 0.1
-python model_evaluate.py --n_workers 4 --noise gaussian --erasing_ratio 0.15
-python model_evaluate.py --n_workers 4 --noise gaussian --erasing_ratio 0.2
